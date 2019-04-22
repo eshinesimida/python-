fed274fa7b874e108b18ed021e5ae71ce41cc6ef
Xianbin Li <454656783@qq.com>
2019-04-22 23:54:03 +0800

    Update NMF.py

--- NMF.py
+++ NMF.py
@@ -55,3 +55,13 @@ for name, estimator in estimators:
     plot_gallery(name, components_[:n_components])
  
 plt.show()
+
+
+import numpy as np
+X = np.array([[1,1,5,2,3], [0,6,2,1,1], [3, 4,0,3,1], [4, 1,5,6,3]])
+from sklearn.decomposition import NMF
+model = NMF(n_components=3, alpha=0.01)
+W = model.fit_transform(X)
+H = model.components_
+print(W)
+print(H)
