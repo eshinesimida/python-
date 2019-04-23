a804012dcfcf135bf1842fda07d602cfb1144d59
Xianbin Li <454656783@qq.com>
2019-04-23 19:37:35 +0800

    Create 高斯混合模型.py

---
+++ 高斯混合模型.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 23 19:29:07 2019
+
+@author: Administrator
+"""
+
+import matplotlib.pyplot as plt
+from sklearn.datasets.samples_generator import make_blobs
+from sklearn.mixture import GaussianMixture
+
+# X为样本特征，Y为样本簇类别， 共1000个样本，每个样本2个特征，共4个簇，簇中心在[-1,-1], [0,0],[1,1], [2,2]
+X, y = make_blobs(n_samples=1000, n_features=2, centers=[[-1,-1], [0,0], [1,1], [2,2]], cluster_std=[0.4, 0.3, 0.4, 0.3], 
+                  random_state = 0)
+
+##设置gmm函数
+gmm = GaussianMixture(n_components=4, covariance_type='full').fit(X)
+##训练数据
+y_pred = gmm.predict(X)
+
+##绘图
+plt.scatter(X[:, 0], X[:, 1], c=y_pred)
+plt.show()
